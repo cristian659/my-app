76033a2804e00449b662a5413c542bce82988846
cristian659 <velascocristian309@gmail.com>
2024-04-13 20:54:51 -0500

    proyecto: my-app descripcion: version final del proyecto my-app

---
+++ src/main/java/com/cristian/myapp/entity/TestEntity.java
@@ -0,0 +1,16 @@
+package com.cristian.myapp.entity;
+
+public class TestEntity {
+
+	private String Entity;
+
+	public String getEntity() {
+		return Entity;
+	}
+
+	public void setEntity(String entity) {
+		Entity = entity;
+	} 
+	
+	
+}
